22f60869a6f86754f4eeb1e2562721da4f60a9e4
jo-arciniegas <arciniegasj626@gmail.com>
2023-02-23 06:30:21 -0700

    added some UI fixes

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent {
-
+  chartData = [
+    {value: Math.floor(Math.random() * 110) , stat: 1},
+    {value: Math.floor(Math.random() * 110) , stat: 2},
+    {value: Math.floor(Math.random() * 110) , stat: 2},
+    {value: Math.floor(Math.random() * 110) , stat: 2},
+    {value: Math.floor(Math.random() * 110) , stat: 1},
+    {value: Math.floor(Math.random() * 110) , stat: 2},
+    {value: Math.floor(Math.random() * 110) , stat: 1},
+  ];
 }
